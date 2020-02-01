9db75afd7e0ea4e751f41fb461e09376ff37a990
abhishek <111995abhishek@gmail.com>
2020-02-01 17:25:38 +0530

    portfolio complete project updated

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/111995abhishek",
+    "name": "Abhishek kr. singh",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/abhishek-kumar-singh-80a60215a/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of both frontend and backend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/abhishek-kumar-singh-80a60215a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/111995abhishek",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am an engineering graduate from New horizon college of engineering,bangalore. And I also completed my full stack developer course at Tarzan Skills,bangalore where i got a hand on experience on python, django,React js, HTML, CSS,Redux etc. i am currently looking job opportunities. ",
+    "address":"India",
+    "website":"https://github.com/111995abhishek",
+    "education":[
+      {
+        "UniversityName":"New Horizon College of Engineering",
+        "specialization":"Information science and Technology",
+        "MonthOfPassing":"july",
+        "YearOfPassing":"2019"
+      },
+      {
+        "UniversityName":"Vishwesariya Institute of Technology",
+        "specialization":"Some specialization"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"MFX Infotech",
+        "specialization":"Robotic Process Automation",
+        "MonthOfLeaving":"may",
+        "YearOfLeaving":"2019",
+        
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"i am good in python, Django, React js, HTML, css, Redux",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Django"
+      },
+      {
+        "skillname":"Redux"
+      }
+      
+    ],
+    "portfolio":[
+      {
+        "name":"Hotel Mangament system",
+        "description":"implemented by using Python, Django Rest framework",
+        
+      },
+      {
+        "name":"usercard",
+        "description":"implemented by using React js",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"shopping website",
+        "description":"implemented by Html,css and javascript",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Abhishek Singh"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Abhishek singh"
+      }
+    ]
+  }
+  
+  export default resumeData
